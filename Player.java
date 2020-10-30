5a256bf3b3dc1c0a1751c3e1817a086d6c56fe6a
Adam Carrol <adamcarrol395@gmail.com>
2020-10-29 21:34:06 -0400

    More CheckMove implementation

--- Player.java
+++ Player.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+import java.util.ArrayList;i
+import java.lang.*;
 
 /* A player is an agent who can perform a move on the board
  * Each player is designated a color and has a list of occupied tiles
@@ -10,48 +11,142 @@ private class Player
 {	
 	
 	private ArrayList<Tile> occupiedTiles;
-	// True = white, False = black
-	private boolean color;
+	private Color color;
+
 
 	//Constructor Goes Here
-	Player(){}
+	Player(Color color, Tile tile){
+		this.color = color;
+		this.occupiedTiles.Add(tile);
+	}
+	
+	public Color getColor(){
+		return this.color;
+	}
+	
+	public ArrayList<Tile> getOccupiedTiles(){
+		return this.occupiedTiles;
+	}
 
 
 	/* Returns True if move is determined Valid, False if Illegal
 	 * If this method returns True, the subsequent Move method is called
 	 *
+	 * @param	player		The Player wishing to move
 	 * @param	current_tile	The tile the Player wishes to move from
 	 * @param	goal_tile	The tile the Player wishes to move to
-	 * @return	valid or invalid move
+	 * @return	boolean		Valid = true, invalid = false
 	 */
-	public boolean CheckMove(Tile current_tile, Tile goal_tile)
-	{	
-
+	public boolean CheckMove(Player player, Tile current_tile, Tile goal_tile)
+	{
+	
 		// Check if goal tile is empty or of same color
-		if((goal_tile.color == Null) or (goal_tile.color == this.color)){
-			return true;
+		if((goal_tile.getColor() == null) or (goal_tile.getColor() == player.getColor())){
+		
 		}else{
 			return false;
 		}
 
-		// Straight Line Conditions
-		if((current_tile.row == goal_tile.row) or (current_tile.col == goal_tile.col)){
-			// Code for tile in same row or column
+		// Check same row
+		if((current_tile.row == goal_tile.row){
+			// Same row & column (cannot move to same tile)
+			if(current_tile.column == goal_tile.column){
+				return false;
+			}
+
+			// Columns: Current < Goal
+			else if(current_tile.column < goal_tile.column){
+				for(i = current_tile.column; i <= goal_tile.column; i++){
+					if((this.board[current_tile.row][i].getColor() == player.getColor()) or (this.board[current_tile.row][i].getColor() = null)){
+						continue;
+					}else{
+						return false;
+					}
+				}
+				// Check count
+				int distance = abs(current_tile.column - goal_tile.column) - 1;
+				int required = (distance(distance + 1))/2;
+				
+				if(current_tile.count < required){
+					return false;
+				}
+
+				return true;
+			}
+			
+			// Columns: Goal < Current
+			else{
+				for(i = current_tile.column; i >= goal_tile.column; i--){
+					if((this.board[current_tile.row][i].getColor() == player.getColor()) or (this.board[current_tile.row][i].getColor() = null)){
+						continue;
+					}else{
+						return false;
+					}
+				}
+				// Check count
+				int distance = abs(current_tile.column - goal_tile.column) - 1;
+				int required = (distance(distance + 1))/2;
+
+				if(current_tile.count < required){
+					return false;
+				}
+
+				return true;
+			}
 		}
-		else if((current_tile.row - goal_tile.row) == (current_tile.col - goal_tile.col)){
+
+		// Check same column
+		else if((current_tile.column == goal_tile.column){
+			// Same column & row (cannot move to same tile)
+			if(current_tile.row == goal_tile.row){
+				return false;
+			}
+
+			// Rows: Current < Goal
+			else if(current_tile.row < goal_tile.row){
+				for(i = current_tile.row; i <= goal_tile.row; i++){
+					if((this.board[i][current_tile.column].getColor() == player.getColor()) || (this.board[i][current_tile.column.getColor() == null)){
+						continue;
+					}else{
+						return false;
+					}
+				}		
+				// Check count
+				int distance = abs(current_tile.row - goal_tile.row) - 1;
+				int required = (distance(distance + 1))/2;
+				
+				if(current_tile.count < required){
+					return false;
+				}
+
+				return true;
+			}
+			
+			// Rows: Goal < Current
+			else{
+				for(i = current_tile.row; i >= goal_tile.row; i--){
+					if((this.board[i][current_tile.column].getColor() == player.getColor()) || (this.board[i][current_tile.column.getColor() == null)){
+						continue;
+					}else{
+						return false;
+					}
+				}			
+				// Check count
+				int distance = abs(current_tile.row - goal_tile.row) - 1;
+				int required = (distance(distance + 1))/2;
+				
+				if(current_tile.count < required){
+					return false;
+				}
+
+				return true;
+		}
+
+		// Check same diagonal
+		else if((current_tile.row - goal_tile.row) == (current_tile.column - goal_tile.column)){
 			// Code for tile in same diagonal
 		}
-
-		// Count condition - Does Player have enough stones in the current tile
-		// to move to the goal tile?
-		if(current_tile.count < /*Some Value*/){
-			return false;
-		}
 		
-		// Need to check if all tiles between curr and goal are
-		// empty or of this.color
-
-
 		// -Debug- Default return false
 		return false;
 	}
